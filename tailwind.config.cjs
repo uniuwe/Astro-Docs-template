fd0eef58e7780f9cff53286a3e953458bc44bd0c
sacarvy <sukhpreetben10@gmail.com>
2022-12-17 08:21:56 +0530

    added scrollbar utilities to tailwindcss, base color for themes, and a background animation utils for move-bg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,29 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: ['./src/**/*.{astro,html,js,jsx,mdx,svelte,ts,tsx}'],
-  theme: {
-    screens: {
+	darkMode: 'class',
+	content: ['./src/**/*.{astro,html,js,jsx,mdx,svelte,ts,tsx}'],
+	theme: {
+		screens: {
 			xxs: '360px',
 			xs: '480px', // => @media (min-width: 480px) { ... }
 			...defaultTheme.screens,
-    },
-    extend: {
-      fontFamily: {
+		},
+		extend: {
+			fontFamily: {
 				sans: ['InterVar', ...defaultTheme.fontFamily.sans],
+			},
+			keyframes: {
+				'move-bg': {
+					to: {
+						backgroundPosition: '400% 0',
+					},
+				},
+			},
+			animation: {
+				'move-bg': 'move-bg 8s infinite linear',
 			},
 			aspectRatio: {
 				auto: 'auto',
@@ -23,13 +34,30 @@ module.exports = {
 				widescreen: '16 / 9',
 				ultrawide: '18 / 5',
 				golden: '1.6180 / 1',
-      },
-      colors: {
-        slate: {          
-					1000: "#0D1423",
-        }
-      }
+			},
+			colors: {
+				slate: {
+					1000: '#0D1423',
+				},
+				base: {
+					...colors.slate,
+				},
+			},
 		},
 	},
-	plugins: [require('@tailwindcss/typography'),require('@tailwindcss/line-clamp')],
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/line-clamp'),
+		function ({ addVariant }) {
+			addVariant(
+				'supports-backdrop-blur',
+				'@supports (backdrop-filter: blur(0)) or (-webkit-backdrop-filter: blur(0))'
+			);
+			addVariant('supports-scrollbars', '@supports selector(::-webkit-scrollbar)');
+			addVariant('children', '& > *');
+			addVariant('scrollbar', '&::-webkit-scrollbar');
+			addVariant('scrollbar-track', '&::-webkit-scrollbar-track');
+			addVariant('scrollbar-thumb', '&::-webkit-scrollbar-thumb');
+		},
+	],
 };
