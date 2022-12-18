31ce39c21ba2ec01c027d6d6f894939e8595d011
sacarvy <sukhpreetben10@gmail.com>
2022-12-18 14:37:51 +0530

    added tailwindcss container-queries

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -52,6 +52,7 @@ module.exports = {
 	plugins: [
 		require('@tailwindcss/typography'),
 		require('@tailwindcss/line-clamp'),
+		require('@tailwindcss/container-queries'),
 		function ({ addVariant }) {
 			addVariant(
 				'supports-backdrop-blur',
